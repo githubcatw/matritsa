3cca81c40b9b07d2f4e3b2359d37bc80fdc07467
githubcatw <17374742+githubcatw@users.noreply.github.com>
2025-04-06 00:49:44 +0400

    add monospace font
    and update the easter egg

---
+++ matritsa/matritsa.Desktop/OfficeCodeProFontCollection.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia;
+using Avalonia.Media.Fonts;
+using System;
+
+namespace matritsa.Desktop {
+    public sealed class OfficeCodeProFontCollection : EmbeddedFontCollection {
+        public OfficeCodeProFontCollection() : base(
+            new Uri("fonts:OfficeCodePro", UriKind.Absolute),
+            new Uri("avares://matritsa/Assets/Fonts", UriKind.Absolute)) {
+        }
+    }
+}
--- matritsa/matritsa.Desktop/Program.cs
+++ matritsa/matritsa.Desktop/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using Avalonia;
+using Avalonia.Media;
 using Avalonia.ReactiveUI;
 using matritsa.Util;
 
@@ -13,20 +14,27 @@ class Program
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args) {
+        var app = BuildAvaloniaApp();
         if (args.Length > 0) {
             if (args[0] == "--follow-the-white-rabbit") {
                 Debug.WriteLine("мы вас поняли, но эта программа - немного другая матрица)");
                 Utils.OpenUrl("https://www.youtube.com/watch?v=pFS4zYWxzNA");
+                app = app.With(new FontManagerOptions() {
+                    DefaultFamilyName = "Office Code Pro",
+                    FontFallbacks = new FontFallback[] {
+                        new FontFallback { FontFamily = "Consolas" },
+                        new FontFallback { FontFamily = "DejaVu Mono" },
+                    },
+                });
             }
         }
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        app.StartWithClassicDesktopLifetime(args);
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            //.WithInterFont()
             .LogToTrace()
             .UseReactiveUI();
 }
